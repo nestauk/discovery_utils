b62eefe46808926e148513fbbfd370b0ed56454e
tomwillcocks1 <thomas.willcocks@hotmail.com>
2024-02-08 11:27:44 +0000

    tidied test files in repo

--- src/getters/my_functions.py
+++
@@ -1,8 +0,0 @@
-def add(number_one, number_two):
-    return number_one + number_two
-
-def divide(number_one, number_two):
-    if number_two == 0:
-        return None
-    else:
-        return number_one / number_two

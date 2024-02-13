85cd91897433dfcac87bfa4d4b35f4a351003228
beingkk <kandersk@gmail.com>
2024-02-13 16:12:45 +0000

    feat: writing enrichments

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+from pathlib import Path
+
+import dotenv
+
+
+dotenv.load_dotenv()
+
+# Define project base directory
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+
+# Define S3 bucket
+S3_BUCKET = os.getenv("BUCKET_NAME_RAW")

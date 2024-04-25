45e61919dad91556666c405690a9e1c0dec66888
Jack Rasala <jack@jrds.io>
2024-04-25 15:24:41 +0100

    Add uploads to utils.s3. Add support for jsonl

--- discovery_utils/utils/s3.py
+++ discovery_utils/utils/s3.py
@@ -12,6 +12,8 @@ import boto3
 import dotenv
 import numpy as np
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 from botocore.client import BaseClient
 
@@ -97,7 +99,6 @@ def _fileobj_to_dict(fileobj: io.BytesIO, **kwargs) -> dict:
 
     Args:
         fileobj (io.BytesIO): Bytes file object.
-        path_from (str): Path of loaded data.
 
     Returns:
         dict: Data as dictionary.
@@ -121,6 +122,8 @@ def _fileobj_to_list(fileobj: io.BytesIO, path_from: str, **kwargs) -> list:
         list_data = fileobj.read().decode().splitlines()
     elif fnmatch(path_from, "*.json"):
         list_data = json.loads(fileobj.getvalue().decode())
+    elif fnmatch(path_from, "*.jsonl"):
+        list_data = [json.loads(line) for line in fileobj]
 
     return list_data
 
@@ -190,6 +193,7 @@ def _download_obj(
             ".csv",
             ".parquet",
             ".json",
+            ".jsonl",
             ".txt",
             ".pkl",
             ".geojson",
@@ -215,18 +219,18 @@ def _download_obj(
             )
     elif download_as == "dict":
         if path_from.endswith((".json",)):
-            return _fileobj_to_dict(fileobj, path_from, **kwargs_reading)
+            return _fileobj_to_dict(fileobj, **kwargs_reading)
         elif path_from.endswith((".geojson",)):
             warnings.warn(
                 "Please check geojson has a member with the name 'type', the value of the member must be one of the following:"
                 "'Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon', 'MultiPolygon', 'GeometryCollection',"
                 "'Feature' and 'FeatureCollection'. Else downloaded dictionary will not be valid geojson."
             )
-            return _fileobj_to_dict(fileobj, path_from, **kwargs_reading)
+            return _fileobj_to_dict(fileobj, **kwargs_reading)
         else:
             raise NotImplementedError("Download as dictionary currently supported only " "for 'json' and 'geojson'.")
     elif download_as == "list":
-        if path_from.endswith((".csv", ".txt", ".json")):
+        if path_from.endswith((".csv", ".txt", ".json", ".jsonl")):
             return _fileobj_to_list(fileobj, path_from, **kwargs_reading)
         else:
             raise NotImplementedError("Download as list currently supported only " "for 'csv', 'txt' and 'json'.")
@@ -240,3 +244,187 @@ def _download_obj(
             "'download_as' not provided. Choose between ('dataframe', 'geodf', "
             "'dict', 'list', 'str', 'np.array'). Not needed for 'pkl files'.'"
         )
+
+
+def _df_to_fileobj(df_data: pd.DataFrame, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert DataFrame into bytes file object.
+
+    Args:
+        df_data (pd.DataFrame): Dataframe to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    buffer = io.BytesIO()
+    if fnmatch(path_to, "*.csv"):
+        df_data.to_csv(buffer, **kwargs)
+    elif fnmatch(path_to, "*.parquet"):
+        df_data.to_parquet(buffer, **kwargs)
+    else:
+        raise NotImplementedError("Uploading dataframe currently supported only for 'csv', 'parquet'.")
+    buffer.seek(0)
+    return buffer
+
+
+def _dict_to_fileobj(dict_data: dict, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert dictionary into bytes file object.
+
+    Args:
+        dict_data (dict): Dictionary to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    buffer = io.BytesIO()
+    if fnmatch(path_to, "*.json"):
+        buffer.write(json.dumps(dict_data, **kwargs).encode())
+    elif fnmatch(path_to, "*.geojson"):
+        if "type" in dict_data:
+            if dict_data["type"] in [
+                "Point",
+                "MultiPoint",
+                "LineString",
+                "MultiLineString",
+                "Polygon",
+                "MultiPolygon",
+                "GeometryCollection",
+                "Feature",
+                "FeatureCollection",
+            ]:
+                buffer.write(json.dumps(dict_data, **kwargs).encode())
+            else:
+                raise AttributeError(
+                    "GeoJSONS must have a member with the name 'type', the value of the member must "
+                    "be one of the following: 'Point', 'MultiPoint', 'LineString', 'MultiLineString',"
+                    "'Polygon', 'MultiPolygon','GeometryCollection', 'Feature' or 'FeatureCollection'."
+                )
+    else:
+        raise NotImplementedError("Uploading dictionary currently supported only for 'json' and 'geojson'.")
+    buffer.seek(0)
+    return buffer
+
+
+def _list_to_fileobj(list_data: list, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert list into bytes file object.
+
+    Args:
+        list_data (list): List to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    buffer = io.BytesIO()
+    if fnmatch(path_to, "*.csv"):
+        pd.DataFrame(list_data).to_csv(buffer, **kwargs)
+    elif fnmatch(path_to, "*.txt"):
+        for row in list_data:
+            buffer.write(bytes(str(row) + "\n", "utf-8"))
+    elif fnmatch(path_to, "*.json"):
+        buffer.write(json.dumps(list_data, **kwargs).encode())
+    else:
+        raise NotImplementedError("Uploading list currently supported only for 'csv', 'txt' and 'json'.")
+    buffer.seek(0)
+    return buffer
+
+
+def _str_to_fileobj(str_data: str, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert str into bytes file object.
+
+    Args:
+        str_data (str): String to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    if fnmatch(path_to, "*.txt"):
+        buffer = io.BytesIO(bytes(str_data.encode("utf-8")))
+    else:
+        raise NotImplementedError("Uploading string currently supported only for 'txt'.")
+    buffer.seek(0)
+    return buffer
+
+
+def _np_array_to_fileobj(np_array_data: np.ndarray, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert numpy array into bytes file object.
+
+    Args:
+        np_array_data (np.ndarray): Numpy array to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    buffer = io.BytesIO()
+    if fnmatch(path_to, "*.csv"):
+        np.savetxt(buffer, np_array_data, delimiter=",", **kwargs)
+    elif fnmatch(path_to, "*.parquet"):
+        pq.write_table(pa.table({"data": np_array_data}), buffer, **kwargs)
+    else:
+        raise NotImplementedError("Uploading numpy array currently supported only for 'csv' and 'parquet.")
+    buffer.seek(0)
+    return buffer
+
+
+def _unsupp_data_to_fileobj(data: any, path_to: str, **kwargs) -> io.BytesIO:
+    """Convert data into bytes file object using pickle file type.
+
+    Args:
+        data (any): Data to convert.
+        path_to (str): Saving file name.
+
+    Returns:
+        io.BytesIO: Bytes file object.
+    """
+    buffer = io.BytesIO()
+    if fnmatch(path_to, "*.pkl"):
+        pickle.dump(data, buffer, **kwargs)
+    else:
+        raise NotImplementedError("This file type is not supported for this data. Use 'pkl' instead.")
+    buffer.seek(0)
+    return buffer
+
+
+def upload_obj(
+    obj: any,
+    bucket: str,
+    path_to: str,
+    kwargs_boto: dict = None,
+    kwargs_writing: dict = None,
+) -> None:
+    """Upload data from memory to S3 location.
+
+    Args:
+        obj (any): Data to upload.
+        bucket (str): Bucket's name.
+        path_to (str): Path location to save data.
+        kwargs_boto (dict, optional): Dictionary of kwargs for boto3 function 'upload_fileobj'.
+        kwargs_writing (dict, optional): Dictionary of kwargs for writing data.
+
+    """
+    if kwargs_boto is None:
+        kwargs_boto = {}
+    if kwargs_writing is None:
+        kwargs_writing = {}
+
+    if isinstance(obj, pd.DataFrame):
+        obj = _df_to_fileobj(obj, path_to, **kwargs_writing)
+    elif isinstance(obj, dict):
+        obj = _dict_to_fileobj(obj, path_to, **kwargs_writing)
+    elif isinstance(obj, list):
+        obj = _list_to_fileobj(obj, path_to, **kwargs_writing)
+    elif isinstance(obj, str):
+        obj = _str_to_fileobj(obj, path_to, **kwargs_writing)
+    elif isinstance(obj, np.ndarray):
+        obj = _np_array_to_fileobj(obj, path_to, **kwargs_writing)
+    else:
+        obj = _unsupp_data_to_fileobj(obj, path_to, **kwargs_writing)
+        warnings.warn(
+            "Data uploaded as pickle. Please consider other accessible " "file types among the supported ones."
+        )
+
+    s3 = boto3.client("s3")
+    s3.upload_fileobj(obj, bucket, path_to, **kwargs_boto)

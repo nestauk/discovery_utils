ca6d7cd1fe2da0ea01a9ca575bddc522898f2ba0
Jack Rasala <jack@jrds.io>
2024-06-19 15:55:07 +0100

    Update inference_bert.py to create label store

--- discovery_utils/horizon_scout/inference_bert.py
+++ discovery_utils/horizon_scout/inference_bert.py
@@ -3,6 +3,7 @@ from typing import List
 import pandas as pd
 import torch
 
+from botocore.exceptions import ClientError
 from tqdm import tqdm
 from transformers import AutoTokenizer
 from transformers import PreTrainedModel
@@ -10,6 +11,7 @@ from transformers import PreTrainedTokenizer
 
 from discovery_utils import logging
 from discovery_utils.getters.horizon_scout import get_bert_model
+from discovery_utils.horizon_scout.utils import get_current_datetime
 from discovery_utils.utils import s3
 
 
@@ -41,6 +43,7 @@ def bert_add_predictions(
     Returns:
         pd.DataFrame: DataFrame with new columns containing classification predictions.
     """
+    df = df.dropna(subset=text_col)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     bert_models = [model.to(device) for model in bert_models]
     total_batches = (len(df) + batch_size - 1) // batch_size
@@ -65,31 +68,89 @@ def bert_add_predictions(
     return df
 
 
-SAVE_PATH = "data/crunchbase/enriched/organizations_new_only_with_bert_preds.csv"
+def preds_to_labels(df: pd.DataFrame, missions: List[str]) -> pd.DataFrame:
+    """
+    Create a new 'label' column based on the values of 'ahl_bert_preds', 'asf_bert_preds', and 'afs_bert_preds'.
+
+    Args:
+        df (pd.DataFrame): Input DataFrame with 'ahl_bert_preds',
+            'asf_bert_preds', and 'afs_bert_preds' columns.
+        missions (List[str]): List of mission names.
+
+    Returns:
+        pd.DataFrame: DataFrame with an additional 'label' column.
+    """
+
+    def make_label(row: pd.Series) -> str:
+        return ",".join([mission for mission in missions if row[f"{mission.lower()}_bert_preds"] == 1])
+
+    df["label"] = df.apply(make_label, axis=1)
+    return df
+
+
+def check_file_exists(path: str) -> bool:
+    """Check if a file exists in an S3 bucket.
+
+    Args:
+        path (str): Path of the file within the bucket.
+
+    Returns:
+        bool: True if the file exists, False otherwise.
+    """
+
+    try:
+        client.head_object(Bucket=s3.BUCKET_NAME_RAW, Key=path)
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+        else:
+            raise e
+
+
 MISSIONS = ["AHL", "ASF", "AFS"]
 MODEL_PATHS = [
-    "AHL_bert_model_0.967_20240601-1441.pkl",
-    "ASF_bert_model_0.93_20240601-1434.pkl",
-    "AFS_bert_model_0.979_20240601-1438.pkl",
+    "AHL_bert_model_0.896_20240618-2156.pkl",
+    "ASF_bert_model_0.925_20240618-2146.pkl",
+    "AFS_bert_model_0.971_20240618-2152.pkl",
 ]
-BATCH_SIZE = 2048
+BATCH_SIZE = 512
+
+LABEL_STORE_PATH = "data/crunchbase/enriched/label_store.csv"
+NEW_ORGS_PATH = "data/crunchbase/enriched/organizations_new_only.parquet"
+FULL_ORGS_PATH = "data/crunchbase/enriched/organizations_full.parquet"
+
+LABEL_TYPE = "BERT classifiers 20240618"
+DATASET_NAME = "Crunchbase"
+
+MODE = "Full"
+MODE = "New"
 
 
 if __name__ == "__main__":
-    logging.info("Downloading new Crunchbase companies")
-    new_companies = s3._download_obj(
+    # Set data path
+    if MODE == "Full":
+        data_path = FULL_ORGS_PATH
+    elif MODE == "New":
+        data_path = NEW_ORGS_PATH
+
+    # Load data
+    logging.info("Downloading Crunchbase companies")
+    companies = s3._download_obj(
         s3_client=client,
         bucket=s3.BUCKET_NAME_RAW,
-        path_from="data/crunchbase/enriched/organizations_new_only.parquet",
+        path_from=data_path,
         download_as="dataframe",
-    )
+    )[["id", "short_description"]]
 
+    # Load models
     bert_models = [get_bert_model(mission, model_path) for mission, model_path in zip(MISSIONS, MODEL_PATHS)]
     tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 
+    # Perform inference
     logging.info("Adding BERT mission predictions")
-    new_companies_with_predictions = bert_add_predictions(
-        df=new_companies,
+    companies = bert_add_predictions(
+        df=companies,
         text_col="short_description",
         bert_models=bert_models,
         tokenizer=tokenizer,
@@ -97,9 +158,28 @@ if __name__ == "__main__":
         batch_size=BATCH_SIZE,
     )
 
-    logging.info("Saving new Crunchbase companies with BERT mission predictions to %s", SAVE_PATH)
-    s3.upload_obj(
-        new_companies_with_predictions,
-        s3.BUCKET_NAME_RAW,
-        SAVE_PATH,
-    )
+    # Convert binary predictions to labels
+    companies = preds_to_labels(companies, MISSIONS)
+
+    # Add label type col
+    companies["label_type"] = LABEL_TYPE
+
+    # Add label date col
+    current_datetime = get_current_datetime()
+    companies["label_date"] = current_datetime
+
+    # Add dataset col
+    companies["dataset"] = DATASET_NAME
+
+    # Append to datastore, if file does not exist then create
+    if check_file_exists(LABEL_STORE_PATH):
+        label_store = s3._download_obj(
+            s3_client=client,
+            bucket=s3.BUCKET_NAME_RAW,
+            path_from=LABEL_STORE_PATH,
+            download_as="dataframe",
+        )
+        label_store = pd.concat([label_store, companies], ignore_index=True).reset_index(drop=True)
+        s3.upload_obj(label_store, s3.BUCKET_NAME_RAW, LABEL_STORE_PATH, kwargs_writing={"index": False})
+    else:
+        s3.upload_obj(companies, s3.BUCKET_NAME_RAW, LABEL_STORE_PATH, kwargs_writing={"index": False})

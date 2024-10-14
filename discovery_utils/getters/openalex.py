bbadfeed17940699a93d19e9944014a46baeeda0
beingkk <kandersk@gmail.com>
2024-10-14 14:10:23 +0100

    docstring

--- discovery_utils/getters/openalex.py
+++ discovery_utils/getters/openalex.py
@@ -12,7 +12,19 @@ def get_openalex_works(
     min_cites: str = ">4",
     n_works: int = 10000,
 ) -> pd.DataFrame:
-    """Get works from OpenAlex API."""
+    """Get works from OpenAlex API.
+
+    The works can be filtered using OpenAlex's relevance score.
+
+    Args:
+        research_question (str): Research question to search for
+        user (str, optional): User email. Defaults to USER_EMAIL.
+        min_cites (str, optional): Minimum number of citations. Defaults to ">4".
+        n_works (int, optional): Number of works to return. Defaults to 10000.
+
+    Returns:
+        pd.DataFrame: DataFrame of works
+    """
     import pyalex
 
     # set pyalex config email

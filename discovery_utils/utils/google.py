60e2fb7a619f2bdf135a28a051a24debba4642f3
beingkk <kandersk@gmail.com>
2025-02-28 17:16:38 +0000

    feat: upload to google slides

--- discovery_utils/utils/google.py
+++ discovery_utils/utils/google.py
@@ -7,23 +7,31 @@ import discovery_child_development.utils.google_utils as google_utils
 # access data from Google Sheets
 data = google_utils.access_google_sheet(<sheet_id>, <sheet_name>)
 """
+
+import os
+
 from os import environ
 from os import path
 from pathlib import PosixPath
 
 import dotenv
-
 import gspread
-from gspread.exceptions import IncorrectCellLabel, WorksheetNotFound
-from df2gspread import gspread2df as g2d
+import gspread_formatting as gsf
+
 from df2gspread import df2gspread as d2g
+from df2gspread import gspread2df as g2d
+from googleapiclient.discovery import Resource
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from gspread.exceptions import WorksheetNotFound
+from oauth2client.service_account import ServiceAccountCredentials
+from pandas import DataFrame
+
 from discovery_utils import PROJECT_DIR
 from discovery_utils import S3_BUCKET
 from discovery_utils import logging
 from discovery_utils.utils.s3 import s3_client
-from oauth2client.service_account import ServiceAccountCredentials
-from pandas import DataFrame
-import gspread_formatting as gsf
+
 
 dotenv.load_dotenv()
 
@@ -68,11 +76,53 @@ def find_credentials(credentials_env_var: str) -> PosixPath:
     return credentials_json
 
 
+# Google Drive
+
+
+def get_drive_service() -> Resource:
+    """Initialise Google Drive API service."""
+    credentials = load_gsheet_credentials()
+    service = build("drive", "v3", credentials=credentials, cache_discovery=False)
+    return service
+
+
+def upload_image_to_drive(service: Resource, file_path: str) -> tuple[str, str]:
+    """Upload a file to Google Drive and makes it public."""
+
+    file_metadata = {"name": os.path.basename(file_path), "mimeType": "image/png"}
+    media = MediaFileUpload(file_path, mimetype="image/png")
+
+    uploaded_file = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+
+    # Make the file publicly accessible
+    service.permissions().create(fileId=uploaded_file["id"], body={"role": "reader", "type": "anyone"}).execute()
+
+    # Get the public URL
+    file_id = uploaded_file["id"]
+    image_url = f"https://drive.google.com/uc?id={file_id}"
+
+    logging.info(f"Uploaded image available at: {image_url}")
+
+    return file_id, image_url
+
+
+def delete_file_from_drive(service: Resource, file_id: str) -> None:
+    """Delete a file from Google Drive using its file ID."""
+    try:
+        service.files().delete(fileId=file_id).execute()
+        logging.info(f"Image with file ID '{file_id}' has been deleted from Google Drive.")
+    except Exception as e:
+        logging.error(f"Error deleting file: {e}")
+
+
+# Google Sheets
+
+
 def load_gsheet_credentials() -> ServiceAccountCredentials:
     """Get credentials for accessing Google Sheets"""
     google_credentials_json = find_credentials("GOOGLE_SHEETS_CREDENTIALS")
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    return ServiceAccountCredentials.from_json_keyfile_name(google_credentials_json, scope)    
+    return ServiceAccountCredentials.from_json_keyfile_name(google_credentials_json, scope)
 
 
 def access_google_sheet(sheet_id: str, sheet_name: str) -> DataFrame:
@@ -122,13 +172,13 @@ def connect_to_gsheet(sheet_id: str) -> gspread.Spreadsheet:
     client = gspread.authorize(credentials)
     # Open the existing Google Sheet by ID
     spreadsheet = client.open_by_key(sheet_id)
-    logging.info(f"Connected to Google Sheet: {spreadsheet.title}")    
+    logging.info(f"Connected to Google Sheet: {spreadsheet.title}")
     return spreadsheet
 
 
 def upload_data_to_gsheet(sheet_id: str, dataframes: dict) -> None:
     """
-    Uploads multiple DataFrames to an existing Google Sheet as separate sheets.
+    Upload multiple DataFrames to an existing Google Sheet as separate sheets.
 
     Args:
         sheet_id (str): The Google Sheet ID where data should be uploaded.
@@ -143,19 +193,19 @@ def upload_data_to_gsheet(sheet_id: str, dataframes: dict) -> None:
         except WorksheetNotFound:
             worksheet = spreadsheet.add_worksheet(title=sheet_name, rows="100", cols="20")
         # Hack to avoid IncorrectCellLabel
-        worksheet.update('A1', [['1']])
-        # Upload data
+        worksheet.update("A1", [["1"]])
+        # Upload data
         logging.info(f"Uploading DataFrame to sheet: {sheet_name}")
         d2g.upload(df, sheet_id, sheet_name, credentials=credentials, row_names=True)
-        # Delete the first column (index)
-        worksheet.delete_columns(1) 
+        # Delete the first column (index)
+        worksheet.delete_columns(1)
     logging.info("Upload completed successfully.")
 
 
 def format_gsheet(sheet_id: str, sheet_name: str, freeze_cols: int = 0) -> None:
     """Apply formatting to a Google Sheet
-    
-    Specifically: freeze the header row, apply background colour to the header row, 
+
+    Specifically: freeze the header row, apply background colour to the header row,
     and add filters.
 
     Args:
@@ -171,11 +221,11 @@ def format_gsheet(sheet_id: str, sheet_name: str, freeze_cols: int = 0) -> None:
 
     # Apply background colour to header row
     header_format = gsf.CellFormat(
-        backgroundColor=gsf.Color(red=1, green=1, blue=0.33), 
+        backgroundColor=gsf.Color(red=1, green=1, blue=0.33),
         textFormat=gsf.TextFormat(bold=True, fontSize=10),
-        horizontalAlignment='LEFT'
+        horizontalAlignment="LEFT",
     )
-    gsf.format_cell_range(worksheet, '1:1', header_format)    
+    gsf.format_cell_range(worksheet, "1:1", header_format)
 
     # Add filters
-    worksheet.set_basic_filter("A1:AZ") 
+    worksheet.set_basic_filter("A1:AZ")
